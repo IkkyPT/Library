d2a65a23e3e9b1a03b10d2d8e74656f9a9715ea0
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-04-01 21:46:27 +0100

    +Add deleteCard() function 1- deleteCard function to remove card from both DOM and myLibrary array, and updated dataset values for remaining cards to reflect the correct index after deletion.

--- library.js
+++ library.js
@@ -1,67 +1,8 @@
 const inputUser = document.querySelector('.input-user');
-const createBookBtn = document.querySelector('crt_book');
+const createBookBtn = document.querySelector('.crt_book');
 let inputBarOn = false;
 let currBook = 0;
-
 let myLibrary = [];
-
-function book(name, author, pages){
-    this.name = name
-    this.author = author
-    this.pages = pages
-}
-
-function addBookToLibrary(book){
-    // Push current book to myLibrary array
-    myLibrary.push(book);
-}
-
-function displayBooks(){
-    const libraryContainer = document.querySelector('.book-grid');
-
-    for(let i = currBook; i < myLibrary.length; i++){
-        const book = myLibrary[i];
-
-        //create a new book card
-        const card = document.createElement('div');
-        card.classList.add('book-card');
-
-        //add the book name, author, and the number of the pages to the card
-        const name = document.createElement('p');
-        name.classList.add('name');
-        name.textContent = `${book.name}`;
-        card.appendChild(name);
-
-        const author = document.createElement('p');
-        author.classList.add('author');
-        author.textContent = `${book.author}`;
-        card.appendChild(author)
-
-        const pages = document.createElement('p');
-        pages.classList.add('pages');
-        pages.textContent = `Pages: ${book.pages}`;
-        card.appendChild(pages);
-
-        // add buttons for "Read" and "Remove"
-        const buttonGroup = document.createElement('div');
-        buttonGroup.classList.add('button-group');
-
-        const readButton = document.createElement('button');
-        readButton.classList.add('btn-read');
-        readButton.textContent = 'Read';
-        buttonGroup.appendChild(readButton);
-
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('btn-delete');
-        deleteButton.textContent = 'Remove';
-        buttonGroup.appendChild(deleteButton);
-
-        card.appendChild(buttonGroup);
-
-        // add the card to the library container
-        libraryContainer.appendChild(card);
-    }
-}
 
 function displayInput(){
     inputBarOn = true;
@@ -71,6 +12,12 @@ function displayInput(){
     else {
         return 0;
     }
+}
+
+function book(name, author, pages){
+    this.name = name
+    this.author = author
+    this.pages = pages
 }
 
 function createBook() {
@@ -99,3 +46,84 @@ function createBook() {
     // increment currBook variable so it display only the new book(s)
     currBook ++;
 };
+
+function addBookToLibrary(book){
+    // Push current book to myLibrary array
+    myLibrary.push(book);
+}
+
+function displayBooks(){
+    const libraryContainer = document.querySelector('.book-grid');
+
+    for(let i = currBook; i < myLibrary.length; i++){
+        const book = myLibrary[i];
+
+        //create a new book card
+        const card = document.createElement('div');
+        card.classList.add('book-card');
+        card.dataset.card = i; // add data attribute to the card with corresponding array value
+
+        //add the book name, author, and the number of the pages to the card
+        const name = document.createElement('p');
+        name.classList.add('name');
+        name.textContent = `${book.name}`;
+        card.appendChild(name);
+
+        const author = document.createElement('p');
+        author.classList.add('author');
+        author.textContent = `${book.author}`;
+        card.appendChild(author)
+
+        const pages = document.createElement('p');
+        pages.classList.add('pages');
+        pages.textContent = `Pages: ${book.pages}`;
+        card.appendChild(pages);
+
+        // add buttons for "Read" and "Remove"
+        const buttonGroup = document.createElement('div');
+        buttonGroup.classList.add('button-group');
+
+        const readButton = document.createElement('button');
+        readButton.classList.add('btn-read');
+        readButton.textContent = 'Read';
+        buttonGroup.appendChild(readButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('btn-delete');
+        deleteButton.setAttribute('onclick','deleteCard()');
+        deleteButton.textContent = 'Remove';
+        buttonGroup.appendChild(deleteButton);
+
+        card.appendChild(buttonGroup);
+
+        // add the card to the library container
+        libraryContainer.appendChild(card);
+    }
+}
+
+function deleteCard() {
+    const deleteBtn = event.target; //Refer to the specific delete button that was clicked
+    const card = deleteBtn.closest('.book-card');
+    const index = card.dataset.card;
+
+    console.log(myLibrary);
+  
+    // remove the book from the myLibrary array
+    myLibrary.splice(index, 1);
+
+  
+    // remove the card from the DOM
+    card.remove();
+
+     // update the dataset values for the remaining cards
+     const cards = document.querySelectorAll('.book-card');
+     for (let i = index; i < cards.length; i++) {
+         const card = cards[i];
+         card.dataset.card = i;
+     }
+    
+    // update currBook to reflect the new number of books displayed
+    currBook = myLibrary.length;
+
+    console.log(myLibrary);
+  }
